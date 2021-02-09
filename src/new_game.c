767401bc0121a92d70cfa9079117c048f40d24e7
J2M2 <juan.martinezmoritz@hotmail.com>
2021-02-09 18:17:49 +0100

    test

--- src/new_game.c
+++ src/new_game.c
@@ -128,7 +128,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(TESTMAP), MAP_NUM(TESTMAP), -1, 6, 4);
+    SetWarpDestination(MAP_GROUP(TESTMAP), MAP_NUM(TESTMAP), -1, 0, 4);
     WarpIntoMap();
 }
 
